65d927658cb303b02647cca745b81c73eed759b5
Joerg Krause <joerg@krause.net>
2018-07-24 21:43:25 +0200

    Server implementation

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # bootstrap4-kurz-und-gut
+
 Die Dateien und Beispiele für die Ausgabe "Bootstrap 4 kurz &amp; gut", erschienen bei O'Reilly 2018.
+
+## Nutzung
+
+Installieren Sie *nodejs* auf Ihrem Rechner. Prüfen Sie in einer Kommandozeile oder im Terminal, dass es funktioniert:
+
+~~~
+node -v
+~~~
+
+Es sollte eine Versionsnummer ausgegeben werden, z.B. "8.11.3".
+
+Gehen Sie in den Ordner, den Sie von Github geklont haben. Geben Sie folgendes ein:
+
+~~~
+npm i
+~~~
+
+Warten Sie, bis all Dateien geladen worden sind. Starten Sie dann den Server:
+
+~~~
+npm start
+~~~
+
+Öffnen Sie einen Browser: *http://localhost:8000*.
+
+## Probleme
+
+Der Port 8000 kann belegt sein. Ändern Sie den Port wie folgt:
+
+* Öffnen Sie die Datei *srv/index.js*
+* Tragen Sie in der Zeile 5 einen anderen Port ein
+
+~~~
+const port = 8000; // hier einen anderen Port eintragen
+~~~
---
+++ srv/index.js
@@ -0,0 +1,146 @@
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const url = require('url'); // built-in utility
+
+const port = 8000;
+const testFolder = './beispiele/';
+
+const htmlTemplate = `<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap kurz &amp; gut</title>
+    <link href="bootstrap.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div class="container">
+    <h1>Bootstrap kurz & gut</h1>
+    <div class="alert alert-info">Die folgende Liste enthält alle Beispieldateien aus dem Buch. Klick öffnet die Datei in einem neuen Fenster.</div>    
+    <h2>Übersicht Dateien</h2>
+    {{}}
+    </div>
+  </body>
+</html>`;
+
+function promisify(fn) {
+  /**
+   * @param {...Any} params The params to pass into *fn*
+   * @return {Promise<Any|Any[]>}
+   */
+  return function promisified(...params) {
+    return new Promise((resolve, reject) => fn(...params.concat([(err, ...args) => err ? reject(err) : resolve( args.length < 2 ? args[0] : args )])))
+  }
+}
+
+class FileReader {
+
+  constructor(folder) {
+    this.fileNames = [];
+    this.readDir(folder).then((d) => {
+      console.log(files.fileNames);
+    });
+  }
+
+  readDir(folder){
+    const readDirAsync = promisify(fs.readdir);
+    return readDirAsync(folder).then(files => {
+        files.forEach(file => {
+          this.fileNames.push(file);
+        });
+    });
+  }
+
+  getMimeType(filePath){
+    let fileExtension = path.extname(filePath);
+    let contentType;
+    switch (fileExtension) {
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.json':
+        contentType = 'application/json';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.woff':
+      case '.woff2':
+        contentType = 'application/font-woff';
+        break;
+      case '.ttf':
+        contentType = 'application/font-sfnt';
+        break;
+      default:
+        contentType = 'text/html';
+    }
+    return contentType;
+  }
+
+  fillTemplate(html){
+    let replacement = this.fileNames.map((file) => {
+      return `<li class="list-group-item"><a href="${file}" target="bt">${file.replace('.html', '')}</a></li>`;
+    });
+    return html.replace('{{}}', `<ul class="list-group">${replacement.join('')}</ul>`);
+  }
+
+  sendError(res){
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('Datei nicht gefunden!');    
+  }
+
+  serveContent() {
+    const server = http.createServer((req, res) => {
+      let contentType = this.getMimeType(req.url);
+      let sanitizedUrl = url.parse(req.url).pathname;
+      switch (sanitizedUrl) {
+        case '/bootstrap.css':
+          let bt = fs.readFileSync(path.join(__dirname, '../node_modules/bootstrap/dist/css/bootstrap.css'));
+          res.write(bt.toString());
+          break;
+        case '/favicon.ico':
+          this.sendError(res);
+          break;
+        case '/':
+        case 'index.html':
+          let filledTemplate = this.fillTemplate(htmlTemplate);
+          res.write(filledTemplate);
+          break;
+        default:
+        if (sanitizedUrl.indexOf('.woff') != -1){
+          let ff = fs.readFileSync(path.join(__dirname, '../', sanitizedUrl));
+          res.writeHead(200, { 'Content-Type': contentType });
+          res.write(ff.toString());
+          break;
+        } 
+        let file = path.join(__dirname, '../beispiele', sanitizedUrl);
+          if (fs.existsSync(file)) {
+            let bf = fs.readFileSync(file);
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.write(bf.toString());
+          } else {
+            let demoFileHelper = path.join(__dirname, '../', sanitizedUrl);
+            if (fs.existsSync(demoFileHelper)){
+              let bf = fs.readFileSync(demoFileHelper);
+              res.writeHead(200, { 'Content-Type': contentType });
+              res.write(bf.toString());
+            } else {
+              this.sendError(res);
+            }
+          }
+          break;
+      }      
+      res.end();
+    });
+    server.on('clientError', (err, socket) => {
+      socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
+    server.listen(port);
+  }
+
+}
+
+let files = new FileReader(testFolder);
+files.serveContent();
+
